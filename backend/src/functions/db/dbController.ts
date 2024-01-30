eac7ea7e9b9786aeac6804a08e0ff2aba5ea9af1
hkxgit <hendrik.koudijs@rahm.de>
2024-01-30 13:30:35 +0100

    init db working patch

--- backend/src/functions/db/dbController.ts
+++ backend/src/functions/db/dbController.ts
@@ -1,27 +1,38 @@
 import {Client, QueryConfig} from 'pg'
-const client = new Client()
+const client = new Client(
+    {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: 'example',
+        port: 5432
+    }
+)
 
 
 export async function dbController(queryTextOrConfig: string | QueryConfig<any[]>, values?: any[] | undefined){
     return new Promise(async (resolve, reject) => {
-        await client.connect()
+        await client.connect().then(async () => {
+            try {
+                if (values) {
+                    const res = await client.query(queryTextOrConfig, values)
+                    resolve(res)
+                    //resolve(res.rows[0].message)
+                } else {
+                    const res = await client.query(queryTextOrConfig)
+                    resolve(res)
+                }
+            } catch (err) {
+                reject(err);
+                await client.end()
+            } finally {
+                await client.end()
+            }
+        })
             .catch((err)=>{
                 reject(err);
             })
-        try {
-            if (values){
-                const res = await client.query(queryTextOrConfig, values)
-                resolve(res)
-                //resolve(res.rows[0].message)
-            } else {
-                const res = await client.query(queryTextOrConfig)
-                resolve(res)
-            }
-        } catch (err) {
-            reject(err);
-        } finally {
-            await client.end()
-        }
+
     })
 
 }
--- backend/src/functions/db/initDB.ts
+++ backend/src/functions/db/initDB.ts
@@ -1,12 +1,12 @@
 import {dbController} from './dbController'
 
-const prompt: string = 'CREATE TABLE Persons (PersonID: int, LastName varchar(255), FirstName varchar(255), Adress varchar(255), City varchar(255))'
+const prompt1: string = 'CREATE TABLE Persons(PersonID int, LastName varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255));';
 
 
 
 export function initDB(){
     return new Promise((resolve, reject)=>{
-        dbController(prompt).then((res)=>{
+        dbController(prompt1).then((res)=>{
             resolve(res)
         }).catch((rej)=>(
             reject(rej)
